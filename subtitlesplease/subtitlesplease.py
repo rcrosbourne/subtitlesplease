b91dc5d917b063f563573d9ea9705196a57ab68b
Rainaldo Crosbourne <rcrosbourne@gmail.com>
2016-07-30 13:34:40 -0500

    some lame tests

--- subtitlesplease/subtitlesplease.py
+++ subtitlesplease/subtitlesplease.py
@@ -1,1 +1,21 @@
 # -*- coding: utf-8 -*-
+import os
+import json
+
+
+class SubtitlesPlease(object):
+    def __init__(self, config=None):
+        if config:
+            fp = open(config, "r")
+            configs = json.load(fp)
+            fp.close()
+            self.directories = configs["watch-directories"]
+            self.files = configs["file-types"]
+        else:
+            config_file = os.path.dirname(os.path.realpath(__file__)) + os.sep + "config.json"
+            fp = open(config_file, "r")
+            configs = json.load(fp)
+            fp.close()
+            self.directories = configs["watch-directories"]
+            self.files = configs["file-types"]
+
--- tests/test_subtitlesplease.py
+++ tests/test_subtitlesplease.py
@@ -8,36 +8,21 @@ test_subtitlesplease
 Tests for `subtitlesplease` module.
 """
 
-
 import sys
 import unittest
-from contextlib import contextmanager
-from click.testing import CliRunner
-
 from subtitlesplease import subtitlesplease
-from subtitlesplease import cli
 
 
-
-class TestSubtitlesplease(unittest.TestCase):
-
+class TestSubtitlesPlease(unittest.TestCase):
     def setUp(self):
-        pass
+        self.subs = subtitlesplease.SubtitlesPlease()
 
     def tearDown(self):
         pass
 
-    def test_000_something(self):
-        pass
-
-    def test_command_line_interface(self):
-        runner = CliRunner()
-        result = runner.invoke(cli.main)
-        assert result.exit_code == 0
-        assert 'subtitlesplease.cli.main' in result.output
-        help_result = runner.invoke(cli.main, ['--help'])
-        assert help_result.exit_code == 0
-        assert '--help  Show this message and exit.' in help_result.output
+    def test_config_setup(self):
+        self.assertEqual(["/Users/smurf/Downloads"], self.subs.directories)
+        self.assertEqual(["*.avi", "*.mp4", "*.mkv"], self.subs.files)
 
 
 if __name__ == '__main__':
